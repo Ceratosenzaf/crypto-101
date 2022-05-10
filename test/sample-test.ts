2c3019dce56dd063c5cf18c91f8fa1b802546006
Davide Cerato <mrc3drik@gmail.com>
2022-05-10 16:57:14 +0200

    converted to typescript

--- test/sample-test.ts
+++ test/sample-test.ts
@@ -1,5 +1,5 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+import { ethers } from "hardhat";
+import { expect } from "chai";
 
 describe("Greeter", () => {
   it("Should return the new greeting once it's changed", async () => {
